c4691eabcadf5a7e05b6e608197965848e47b371
shigemk2 <i.am.shige@gmail.com>
2013-10-12 02:50:42 +0900

    Add scroll

--- app/web_view_controller.rb
+++ app/web_view_controller.rb
@@ -5,21 +5,22 @@ class WebViewController < UIViewController
     super
     self.navigationItem.title = self.item[3]
     @webview = UIWebView.new.tap do |v|
+      scrollwindow = UIScrollView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+      scrollwindow.scrollEnabled = true
+      scrollwindow.delegate = self
+
       v.frame = self.view.bounds
       v.scalesPageToFit = true
       url = self.item[9].gsub(/\"/, "") # image
       image_src = NSData.dataWithContentsOfURL(NSURL.URLWithString(url))
       image = UIImage.imageWithData(image_src)
 
-      # alert = UIAlertView.new
-      # alert.message = "#{image.size.height}"
-      # alert.addButtonWithTitle 'OK'
-      # alert.show
-
       image_view = UIImageView.alloc.initWithImage(image)
       image_view.frame = CGRectMake(0, 100, 320, 320)
+      scrollwindow.addSubview(image_view)
+      scrollwindow.contentSize = image_view.size
       v.delegate = self
-      view.addSubview(image_view)
+      view.addSubview(scrollwindow)
 
       # v.loadHTMLString(self.item["body"], baseURL:nil)
     end
